40e1f94168dacc5b5cd4dc86348207590a71cd8e
f549263766 <549263766@qq.com>
2023-03-11 15:25:27 +0800

    feat: newly increased a set of wrappers for dataset

--- modules/datasets/__init__.py
+++ modules/datasets/__init__.py
@@ -1,5 +1,8 @@
 from .base_dataset import BaseDataset
 from .mnist import MNIST, FashionMNIST
 from .cifar import CIFAR10, CIFAR100
+from .custom import CustomDataset
+from .dataset_wrappers import ConcatDataset, RepeatDataset, ClassBalanceDatasets, KFoldDataset
 
-__all__ = ['BaseDataset', 'MNIST', 'FashionMNIST', 'CIFAR10', 'CIFAR100']
+__all__ = ['BaseDataset', 'MNIST', 'FashionMNIST', 'CIFAR10', 'CIFAR100', 'CustomDataset',
+           'ConcatDataset', 'RepeatDataset', 'ClassBalanceDatasets', 'KFoldDataset']
