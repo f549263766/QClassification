7df191abfbbbb68daf62b97a9f8b7501f06f3b72
f549263766 <549263766@qq.com>
2023-03-24 18:01:36 +0800

    feat: add transforms functions

--- modules/datasets/pipelines/__init__.py
+++ modules/datasets/pipelines/__init__.py
@@ -1,3 +1,11 @@
 from .compose import Compose
+from .formatting import (Collect, ImageToTensor, ToNumpy, ToPIL, ToTensor, Transpose, to_tensor)
+from .loading import LoadImageFromFile
+from .transforms import (CenterCrop, ColorJitter, Lighting, Normalize, Pad, RandomCrop,
+                         RandomErasing, RandomFlip, RandomGrayscale, RandomResizeCrop,
+                         Resize)
 
-__all__ = ['Compose']
+__all__ = ['Compose', 'LoadImageFromFile', 'to_tensor',
+           'Collect', 'ImageToTensor', 'ToNumpy', 'ToPIL', 'ToTensor', 'Transpose',
+           'CenterCrop', 'ColorJitter', 'Lighting', 'Normalize', 'Pad', 'RandomCrop',
+           'RandomErasing', 'RandomFlip', 'RandomGrayscale', 'RandomResizeCrop', 'Resize']
